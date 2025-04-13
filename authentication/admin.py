a997234ec875710d1eb92734e25e2b041c39f0fa
Dominik <101473446+b14ucky@users.noreply.github.com>
2025-04-13 17:24:37 +0200

    feat: enhance CustomUserCreationForm to automatically assign users to the Employees group

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import Group
 from .models import CustomUser
 
 
@@ -8,6 +9,17 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("email", "first_name", "last_name")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        employees_group = Group.objects.filter(name="Employees")
+        self.cleaned_data["groups"] = self.cleaned_data["groups"] | employees_group
+
+        if commit:
+            user.save()
+
+        return user
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -53,6 +65,7 @@ class CustomUserAdmin(BaseUserAdmin):
                     "last_name",
                     "password1",
                     "password2",
+                    "groups",
                     "is_staff",
                     "is_superuser",
                 ),
